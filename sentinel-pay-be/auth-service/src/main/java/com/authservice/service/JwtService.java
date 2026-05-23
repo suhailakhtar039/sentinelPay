04f969321bab459bda2f89de166531e1b70c4037
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-23 19:56:08 +0530

    generation validation along with extracted username is present

---
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/service/JwtService.java
@@ -0,0 +1,74 @@
+package com.authservice.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String KEY = "mySuperSecretKeyForJwtToken123456";
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init(){
+        secretKey = Keys.hmacShaKeyFor(KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Generate token with the help of username only.
+     * Take care of claims thoroughly
+     */
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 60*60*1000))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /**
+     * Validate token. Check username and expiration date.
+     */
+    public boolean validateToken(String token, String username){
+        String extractedUsername = extractUsername(token);
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+
+    /**
+     * Extract username. Username is subject in all cases
+     */
+    public String extractUsername(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+
+    /**
+     * Check expiration of token
+     */
+    private boolean isTokenExpired(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration()
+                .before(new Date());
+    }
+
+    /**
+     * extract all claims to avoid reusability
+     */
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
