dad9d2d51f59a53e944289844099c987ecd54e85
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-23 20:37:07 +0530

    UserRepo created

---
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.authservice.repository;
+
+import com.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
