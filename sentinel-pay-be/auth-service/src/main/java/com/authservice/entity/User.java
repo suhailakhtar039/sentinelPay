fbc2fd26cc4aa8d18172decf8d1b71ef6cfee14d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-24 22:28:30 +0530

    fix column with role

--- sentinel-pay-be/auth-service/src/main/java/com/authservice/entity/User.java
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/entity/User.java
@@ -1,7 +1,10 @@
 package com.authservice.entity;
 
+import com.authservice.dto.RoleEnum;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,5 +35,6 @@ public class User {
 
     private String password;
 
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private RoleEnum role;
 }
