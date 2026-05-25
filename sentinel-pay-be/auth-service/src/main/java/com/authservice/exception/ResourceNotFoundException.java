88b3c5ffa35696deb234d324f396af53a19f496b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-25 22:41:02 +0530

    global exception handler

---
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.authservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
