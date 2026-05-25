9af19ee4d9a4b503f554965a8d711c8b7a9b078e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-25 22:35:59 +0530

    validation annotation used

--- sentinel-pay-be/auth-service/src/main/java/com/authservice/dto/LoginRequest.java
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/dto/LoginRequest.java
@@ -1,9 +1,14 @@
 package com.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @Email(message = "Enter proper email")
     private String email;
+
+    @NotBlank(message = "Enter password")
     private String password;
 }
--- sentinel-pay-be/auth-service/src/main/java/com/authservice/dto/RegisterRequest.java
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/dto/RegisterRequest.java
@@ -1,10 +1,17 @@
 package com.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Enter proper email")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
