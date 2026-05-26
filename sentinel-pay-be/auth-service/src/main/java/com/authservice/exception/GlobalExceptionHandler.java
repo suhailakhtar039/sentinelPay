63442028f1976f015aa0e7ee40c0383f3198f4e6
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-26 19:13:55 +0530

    global exception handler modified

--- sentinel-pay-be/auth-service/src/main/java/com/authservice/exception/GlobalExceptionHandler.java
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/exception/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.authservice.exception;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -36,5 +40,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .badRequest()
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>>
+    handleValidation(
+            MethodArgumentNotValidException ex
+    ) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        ));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
