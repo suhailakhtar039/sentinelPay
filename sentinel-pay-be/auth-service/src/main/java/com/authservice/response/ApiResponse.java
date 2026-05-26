a7959d7b99ea3eb338fecb155a5faba47da9e97a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-26 19:15:11 +0530

    ApiResponse created as well

---
+++ sentinel-pay-be/auth-service/src/main/java/com/authservice/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.authservice.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+}
