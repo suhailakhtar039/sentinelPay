8a663efd6f5452d49bcf5f6f4be41ce7aff13663
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-29 16:18:27 +0530

    consumer created in wallet service

---
+++ sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/kafka/UserEventConsumer.java
@@ -0,0 +1,27 @@
+package com.walletservice.kafka;
+
+import com.walletservice.dto.CreateWalletRequest;
+import com.walletservice.event.UserRegisteredEvent;
+import com.walletservice.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserEventConsumer {
+    private final WalletService walletService;
+
+    @KafkaListener(topics = KafkaTopics.USER_REGISTERED, groupId = "wallet-group")
+    public void consumeUserRegisteredEvent(UserRegisteredEvent event) {
+        log.info("Received USER_REGISTERED event for userId:{}", event.getUserId());
+        CreateWalletRequest request = CreateWalletRequest.builder()
+                .userId(event.getUserId())
+                .build();
+
+        walletService.createWallet(request);
+    }
+
+}
