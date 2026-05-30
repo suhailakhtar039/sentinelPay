4146846ea813ab99cedd94ad04c375aac2815744
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-30 17:38:18 +0530

    UserRegisteredEvent added in common-lib

--- sentinel-pay-be/common-lib/src/main/java/com/sentinelpay/common/event/UserRegisteredEvent.java
+++ sentinel-pay-be/common-lib/src/main/java/com/sentinelpay/common/event/UserRegisteredEvent.java
@@ -1,11 +1,13 @@
-package com.authservice.event;
+package com.sentinelpay.common.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
