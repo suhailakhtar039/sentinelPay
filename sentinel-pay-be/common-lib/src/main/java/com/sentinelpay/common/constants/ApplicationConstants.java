f867ce916e830a98f5e41929d4b4fccd9bc9ed7a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-31 12:15:38 +0530

    INR added in constants

---
+++ sentinel-pay-be/common-lib/src/main/java/com/sentinelpay/common/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.sentinelpay.common.constants;
+
+public class ApplicationConstants {
+    private ApplicationConstants() {
+    }
+
+    public static final String DEFAULT_CURRENCY = "INR";
+}
