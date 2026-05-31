d0b28b59407eef841669f5b9bb8f80895e666884
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-31 12:38:49 +0530

    refactored

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/PaymentserviceApplication.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/PaymentserviceApplication.java
@@ -1,4 +1,4 @@
-package com.payment.paymentservice;
+package com.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sentinel-pay-be/payment-service/src/test/java/com/paymentservice/PaymentserviceApplicationTests.java
+++ sentinel-pay-be/payment-service/src/test/java/com/paymentservice/PaymentserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.payment.paymentservice;
+package com.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
