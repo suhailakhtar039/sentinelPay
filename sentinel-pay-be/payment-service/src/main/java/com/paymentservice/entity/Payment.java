0a4e1997af7161328dbb3f30ce1fc7df17b74870
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-31 12:37:45 +0530

    entity created

---
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/entity/Payment.java
@@ -0,0 +1,42 @@
+package com.paymentservice.entity;
+
+import com.paymentservice.enums.PaymentStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    private Long senderUserId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
