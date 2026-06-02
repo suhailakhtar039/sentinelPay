5e89d3ab010a5478280ffb0c7d6927452dd3f8ea
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-02 19:20:24 +0530

    proper annotation event

--- sentinel-pay-be/common-lib/src/main/java/com/sentinelpay/common/event/PaymentInitiatedEvent.java
+++ sentinel-pay-be/common-lib/src/main/java/com/sentinelpay/common/event/PaymentInitiatedEvent.java
@@ -1,12 +1,16 @@
 package com.sentinelpay.common.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentInitiatedEvent {
 
     private Long paymentId;
