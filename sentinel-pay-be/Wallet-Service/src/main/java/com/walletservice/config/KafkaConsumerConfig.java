1a2839edcd22c0f9c24dbe975e943ecdb8da6f0f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-02 19:15:00 +0530

    create serializer and deserializer via bean

--- sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/config/KafkaConsumerConfig.java
+++ sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/config/KafkaConsumerConfig.java
@@ -1,18 +1,108 @@
 package com.walletservice.config;
 
+import com.sentinelpay.common.event.PaymentInitiatedEvent;
+import com.sentinelpay.common.event.UserRegisteredEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
     @Bean
-    public DefaultErrorHandler errorHandler(){
+    public DefaultErrorHandler errorHandler() {
         FixedBackOff backOff = new FixedBackOff(
                 2000L,
                 3
         );
         return new DefaultErrorHandler(backOff);
     }
+
+    @Bean
+    public ConsumerFactory<String, UserRegisteredEvent> userConsumerFactory() {
+
+        JsonDeserializer<UserRegisteredEvent> deserializer =
+                new JsonDeserializer<>(UserRegisteredEvent.class);
+
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "wallet-user-group"
+        );
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UserRegisteredEvent>
+    userKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, UserRegisteredEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(userConsumerFactory());
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, PaymentInitiatedEvent> paymentConsumerFactory() {
+
+        JsonDeserializer<PaymentInitiatedEvent> deserializer =
+                new JsonDeserializer<>(PaymentInitiatedEvent.class);
+
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "wallet-payment-group"
+        );
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentInitiatedEvent>
+    paymentKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, PaymentInitiatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(paymentConsumerFactory());
+
+        return factory;
+    }
+
 }
