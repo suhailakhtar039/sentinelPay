22ec09f4a26f670eb0332ba6349cd1d93ee036c4
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-03 19:13:25 +0530

    try catch block created for simulating failed behavior

--- sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/kafka/PaymentInitiatedConsumer.java
+++ sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/kafka/PaymentInitiatedConsumer.java
@@ -1,6 +1,9 @@
 package com.walletservice.kafka;
 
+import com.sentinelpay.common.event.PaymentCompletedEvent;
+import com.sentinelpay.common.event.PaymentFailedEvent;
 import com.sentinelpay.common.event.PaymentInitiatedEvent;
+import com.sentinelpay.common.exception.BadRequestException;
 import com.sentinelpay.common.kafka.KafkaTopics;
 import com.walletservice.service.WalletService;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +17,33 @@ import org.springframework.stereotype.Component;
 public class PaymentInitiatedConsumer {
 
     private final WalletService walletService;
+    private final WalletEventProducer walletEventProducer;
 
     @KafkaListener(topics = KafkaTopics.PAYMENT_INITIATED, groupId = "wallet-group")
     public void consume(PaymentInitiatedEvent event) {
-        log.info("Received payment event:{}", event.getPaymentId());
-        // Debit service
-        walletService.debit(event.getSenderUserId(), event.getAmount());
+        try {
+            log.info("Received payment event:{}", event.getPaymentId());
+            // Debit service
+            walletService.debit(event.getSenderUserId(), event.getAmount());
 
-        // Credit service
-        walletService.credit(event.getReceiverUserId(), event.getAmount());
+            // Credit service
+            walletService.credit(event.getReceiverUserId(), event.getAmount());
+
+            // Wallet event producer baad me aaya payment complete batane ke liye
+            walletEventProducer.publishPaymentCompleted(PaymentCompletedEvent.builder()
+                    .paymentId(event.getPaymentId())
+                    .senderUserId(event.getSenderUserId())
+                    .receiverUserId(event.getReceiverUserId())
+                    .build()
+            );
+        } catch (BadRequestException e) {
+            log.info("Payment id:{}", event.getPaymentId());
+            walletEventProducer.publishPaymentFailed(PaymentFailedEvent.builder()
+                    .paymentId(event.getPaymentId())
+                    .reason(e.getMessage())
+                    .build()
+            );
+        }
     }
 
 }
