f2fc9b4b7d1268229ade5bde92038419e6f40e3b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-05 21:30:58 +0530

    LedgerEntry entity created

---
+++ sentinel-pay-be/ledger-service/src/main/java/com/ledgerservice/entity/LedgerEntry.java
@@ -0,0 +1,45 @@
+package com.ledgerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ledger_entries")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LedgerEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long entryId;
+
+    private Long paymentId;
+
+    private Long senderUserId;
+
+    private Long receiverUserId;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false, length = 3)
+    private String currency;
+
+    @Column(nullable = false)
+    private LocalDateTime transactionTime;
+}
