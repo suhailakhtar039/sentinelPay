331347c88a7cfd19dfe6a338cf168997829a4583
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-05 21:18:35 +0530

    create ledger service

---
+++ sentinel-pay-be/ledger-service/src/test/java/com/ledgerservide/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgerservide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
