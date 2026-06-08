1f24b4128a35bddaed62cdbf7b254fe7a7ec1830
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-08 21:18:13 +0530

    interceptor created

--- sentinel-pay-fe/src/app/core/interceptors/auth-interceptor.ts
+++ sentinel-pay-fe/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenStorageService } from '../services/token-storage';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const tokenStorage = inject(TokenStorageService);
+
+  const token = tokenStorage.getToken();
+  if (!token) {
+    return next(req);
+  }
+
+  const clonedRequest = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+  return next(clonedRequest);
 };
