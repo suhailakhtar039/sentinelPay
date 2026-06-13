25ccff4b42f6ad13446904b0451b58783bf352e4
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-13 16:06:38 +0530

    imported all important modules

--- sentinel-pay-fe/src/app/features/auth/login-component/login-component.ts
+++ sentinel-pay-fe/src/app/features/auth/login-component/login-component.ts
@@ -1,24 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-component',
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardTitle,
+    MatCardSubtitle,
+  ],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
 export class LoginComponent implements OnInit {
-  formForLogin!: FormGroup;
+  hidePassword = true;
+  loginForm!: FormGroup;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.formForLogin = this.fb.group({
+    this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onSubmit() {
-    console.log(this.formForLogin);
+    console.log(this.loginForm);
   }
 }
