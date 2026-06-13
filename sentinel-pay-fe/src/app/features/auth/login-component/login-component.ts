0fab67ebad46e29f2c05ac9f38e34a5043f311b1
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-13 13:22:20 +0530

    login component is being modified

--- sentinel-pay-fe/src/app/features/auth/login-component/login-component.ts
+++ sentinel-pay-fe/src/app/features/auth/login-component/login-component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formForLogin!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.formForLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formForLogin);
+  }
 }
