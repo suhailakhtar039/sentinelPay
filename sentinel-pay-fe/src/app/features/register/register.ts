070d1a1197a4d72ec30b95c95503749cdab2352b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-15 19:08:38 +0530

    create register component

---
+++ sentinel-pay-fe/src/app/features/register/register.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardTitle,
+    MatCardSubtitle,
+    RouterModule,
+  ],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  registerForm!: FormGroup;
+
+  hidePassword = true;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {}
+}
