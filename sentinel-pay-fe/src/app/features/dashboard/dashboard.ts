856cc7ee156d37775d77863fc240bef06201a429
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-15 21:27:05 +0530

    extracting useful information

--- sentinel-pay-fe/src/app/features/dashboard/dashboard.ts
+++ sentinel-pay-fe/src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from '../../core/services/token-storage';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  email!: string | null;
+  role!: string | null;
 
+  constructor(
+    private tokenService: TokenStorageService,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.email = this.tokenService.getEmail();
+    this.role = this.tokenService.getRole();
+  }
+
+  logout() {
+    this.tokenService.logout();
+    this.router.navigate(['/login']);
+  }
 }
