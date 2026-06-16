8f053c70743aaf7052224a1a1b2068f66cfc3a7d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-16 23:19:55 +0530

    create main-layout component

--- sentinel-pay-fe/src/app/features/main-layout/main-layout.ts
+++ sentinel-pay-fe/src/app/features/main-layout/main-layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Navbar } from '../navbar/navbar';
+import { Sidebar } from '../sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [MatToolbarModule, Navbar, Sidebar, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
-export class MainLayout {
-
-}
+export class MainLayout {}
