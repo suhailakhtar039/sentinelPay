4a80e8208774eeceec957c5ff7f8471e4213fb34
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-16 23:18:21 +0530

    route updated

--- sentinel-pay-fe/src/app/app.routes.ts
+++ sentinel-pay-fe/src/app/app.routes.ts
@@ -1,23 +1,50 @@
 import { Route } from '@angular/router';
+
 import { LoginComponent } from './features/auth/login-component/login-component';
 import { authGuard } from './core/guards/auth-guard';
+import { MainLayout } from './features/main-layout/main-layout';
 
 export const routes: Route[] = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
     component: LoginComponent,
   },
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
-  },
+
   {
     path: 'register',
     loadComponent: () => import('./features/register/register').then((m) => m.Register),
   },
+
+  {
+    path: '',
+    component: MainLayout,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+
+      {
+        path: 'payments',
+        loadComponent: () => import('./features/payment/payment').then((m) => m.Payment),
+      },
+
+      {
+        path: 'ledger',
+        loadComponent: () => import('./features/ledger/ledger').then((m) => m.Ledger),
+      },
+    ],
+  },
+
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'login',
   },
 ];
