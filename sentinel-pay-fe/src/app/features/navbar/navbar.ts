6bae3f140471a2d59b47e1ec53212cc3af232c5f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-16 23:20:40 +0530

    logout method created

--- sentinel-pay-fe/src/app/features/navbar/navbar.ts
+++ sentinel-pay-fe/src/app/features/navbar/navbar.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { TokenStorageService } from '../../core/services/token-storage';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -22,8 +23,17 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class Navbar {
   email!: string | null;
-  constructor(private tokenService: TokenStorageService) {}
+  constructor(
+    private tokenService: TokenStorageService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
   ngOnInit() {
     this.email = this.tokenService.getEmail();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
