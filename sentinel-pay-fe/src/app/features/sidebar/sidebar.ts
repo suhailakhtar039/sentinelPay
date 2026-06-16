16cf90a0aa5749710b21faf8694c72c83447d140
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-16 23:21:25 +0530

    proper modules imported

--- sentinel-pay-fe/src/app/features/sidebar/sidebar.ts
+++ sentinel-pay-fe/src/app/features/sidebar/sidebar.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule,
+    MatListModule,
+    RouterOutlet,
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {
-
-}
+export class Sidebar {}
