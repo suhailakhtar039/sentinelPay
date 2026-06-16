58eda7b2d12b98cbb2e7ba361fab3cbf0a6789c6
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-16 21:42:20 +0530

    minal navbar

--- sentinel-pay-fe/src/app/features/navbar/navbar.ts
+++ sentinel-pay-fe/src/app/features/navbar/navbar.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { TokenStorageService } from '../../core/services/token-storage';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterLink,
+    MatButtonModule,
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  email!: string | null;
+  constructor(private tokenService: TokenStorageService) {}
+  ngOnInit() {
+    this.email = this.tokenService.getEmail();
+  }
 }
