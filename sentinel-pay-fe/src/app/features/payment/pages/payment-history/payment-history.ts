278d5349fe508055df5984040c2644cfd4794cf9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-22 22:56:34 +0530

    create component payment-history

---
+++ sentinel-pay-fe/src/app/features/payment/pages/payment-history/payment-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-history',
+  imports: [],
+  templateUrl: './payment-history.html',
+  styleUrl: './payment-history.css',
+})
+export class PaymentHistory {
+
+}
