4d43235bb59996c15ed829a2a63e9dde84691546
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-22 19:07:54 +0530

    create ledger service

--- sentinel-pay-fe/src/app/core/services/ledger-service.ts
+++ sentinel-pay-fe/src/app/core/services/ledger-service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../../shared/models/api-response';
+import { LedgerResponse } from '../../shared/models/ledger-response';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LedgerService {
-  
+  private readonly http = inject(HttpClient);
+
+  getMyTransactions(): Observable<ApiResponse<LedgerResponse[]>> {
+    return this.http.get<ApiResponse<LedgerResponse[]>>(`${API_CONFIG.ledger}/me`);
+  }
 }
