eb0133bc4cce93e9e2bbbe188c3bf5d064bed817
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-22 22:24:40 +0530

    refactored payment related thing into payment

--- sentinel-pay-fe/src/app/shared/models/payment-response.ts
+++
@@ -1,9 +0,0 @@
-export interface PaymentResponse {
-  paymentId: number;
-  senderUserId: number;
-  receiverUserId: number;
-  amount: number;
-  status: string;
-  createdAt: string;
-  failureReason: string | null;
-}
