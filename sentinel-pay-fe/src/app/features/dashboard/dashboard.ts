31bb584262d80cf2338aed6941e5160ef7ed6e2a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-22 19:18:11 +0530

    use forkjoin for total synchronous data from walletService, paymentService and ledgerService

--- sentinel-pay-fe/src/app/features/dashboard/dashboard.ts
+++ sentinel-pay-fe/src/app/features/dashboard/dashboard.ts
@@ -3,6 +3,13 @@ import { TokenStorageService } from '../../core/services/token-storage';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { WalletResponse } from '../../shared/models/wallet-response';
+import { PaymentResponse } from '../../shared/models/payment-response';
+import { LedgerResponse } from '../../shared/models/ledger-response';
+import { forkJoin } from 'rxjs';
+import { WalletService } from '../../core/services/wallet-service';
+import { LedgerService } from '../../core/services/ledger-service';
+import { PaymentService } from '../../core/services/payment-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,14 +21,36 @@ export class Dashboard {
   email!: string | null;
   role!: string | null;
 
+  wallet?: WalletResponse;
+  payments: PaymentResponse[] = [];
+  transactions: LedgerResponse[] = [];
+
   constructor(
     private tokenService: TokenStorageService,
     private router: Router,
+    private walletService: WalletService,
+    private ledgerService: LedgerService,
+    private paymentService: PaymentService,
   ) {}
 
   ngOnInit() {
     this.email = this.tokenService.getEmail();
     this.role = this.tokenService.getRole();
+
+    forkJoin({
+      wallet: this.walletService.getMyWallet(),
+      payments: this.paymentService.getMyPayments(),
+      ledger: this.ledgerService.getMyTransactions(),
+    }).subscribe({
+      next: (result: any) => {
+        ((this.wallet = result.wallet.data),
+          (this.payments = result.payments.data),
+          (this.transactions = result.ledger.data));
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
   }
 
   logout() {
