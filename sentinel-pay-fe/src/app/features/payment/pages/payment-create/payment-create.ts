e39c37fb2d24ca7ce5135381bfc2f664e191cfe6
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-23 22:32:01 +0530

    submit functionality created

--- sentinel-pay-fe/src/app/features/payment/pages/payment-create/payment-create.ts
+++ sentinel-pay-fe/src/app/features/payment/pages/payment-create/payment-create.ts
@@ -8,6 +8,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { PaymentService } from '../../services/payment-service';
+import { CreatePaymentRequest } from '../../model/create-payment-request';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-payment-create',
@@ -36,4 +38,37 @@ export class PaymentCreate {
     receiverUserId: ['', [Validators.required, Validators.min(1)]],
     amount: ['', [Validators.required, Validators.min(1)]],
   });
+
+  submit() {
+    if (this.paymentForm.invalid) {
+      this.paymentForm.markAllAsTouched();
+      return;
+    }
+    this.loading = true;
+
+    const request: CreatePaymentRequest = {
+      receiverUserId: Number(this.paymentForm.value.receiverUserId),
+      amount: Number(this.paymentForm.value.amount),
+    };
+
+    this.paymentService
+      .createPayment(request)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        }),
+      )
+      .subscribe({
+        next: (response) => {
+          this.snackBar.open(response.message || 'Payment Initiated Successfully', 'Close', {
+            duration: 3000,
+          });
+          this.paymentForm.reset();
+        },
+        error: (err: any) => {
+          const message = err?.error?.message || 'Unable To Process Payment';
+          this.snackBar.open(message, 'Close', { duration: 5000 });
+        },
+      });
+  }
 }
