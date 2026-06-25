d8202ca8c8c3286bca20615441a3fb5ec735cf76
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-25 19:22:01 +0530

    create wallet component

---
+++ sentinel-pay-fe/src/app/features/wallet/wallet.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet',
+  imports: [],
+  templateUrl: './wallet.html',
+  styleUrl: './wallet.css',
+})
+export class WalletComponent {}
