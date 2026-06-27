a0c93c02377acb09c107a4f13056fc0ff0e50533
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-27 17:29:47 +0530

    create shell component

--- sentinel-pay-fe/src/app/features/payment/payment.ts
+++ sentinel-pay-fe/src/app/features/payment/payment.ts
@@ -3,18 +3,13 @@ import { Component, inject } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-payment',
-  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './payment.html',
   styleUrl: './payment.css',
 })
-export class Payment {
-  private readonly router = inject(Router);
-
-  get showMenu(): boolean {
-    return this.router.url === '/payments';
-  }
-}
+export class Payment {}
