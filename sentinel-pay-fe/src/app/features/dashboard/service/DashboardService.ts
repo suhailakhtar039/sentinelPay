e9d663cec092845e4fafb070922cb81db4849b1f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-06-29 21:47:29 +0530

    create service dashboard

---
+++ sentinel-pay-fe/src/app/features/dashboard/service/DashboardService.ts
@@ -0,0 +1,62 @@
+import { ChangeDetectorRef, Injectable } from '@angular/core';
+import { WalletService } from '../../../core/services/wallet-service';
+import { LedgerService } from '../../../core/services/ledger-service';
+import { PaymentService } from '../../payment/services/payment-service';
+import { forkJoin, map, Observable } from 'rxjs';
+import { WalletResponse } from '../../../shared/models/wallet-response';
+import { LedgerResponse } from '../../../shared/models/ledger-response';
+import { PaymentResponse } from '../../payment/model/payment-response';
+import { PaymentStatus } from '../../payment/model/payment-status';
+import { ApiResponse } from '../../../shared/models/api-response';
+import { DashboardSummary } from '../model/dashboard-summary';
+
+interface DashboardApiResponse {
+  wallet: ApiResponse<WalletResponse>;
+  payments: ApiResponse<PaymentResponse[]>;
+  ledger: ApiResponse<LedgerResponse[]>;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  constructor(
+    private walletService: WalletService,
+    private ledgerService: LedgerService,
+    private paymentService: PaymentService,
+  ) {}
+
+  getDashboardSummary(): Observable<DashboardSummary> {
+    return forkJoin({
+      wallet: this.walletService.getMyWallet(),
+      payments: this.paymentService.getMyPayments(),
+      ledger: this.ledgerService.getMyTransactions(),
+    }).pipe(
+      map((result: DashboardApiResponse) => {
+        const wallet = result.wallet.data;
+        const payments = result.payments.data;
+        const transactions = result.ledger.data;
+
+        return {
+          wallet,
+          payments,
+          transactions,
+
+          walletBalance: Number(wallet.balance),
+
+          totalPayments: payments.length,
+
+          successfulPayments: payments.filter(
+            (payment) => payment.status === PaymentStatus.COMPLETED,
+          ).length,
+
+          failedPayments: payments.filter(
+            (payment) =>
+              payment.status === PaymentStatus.FAILED ||
+              payment.status === PaymentStatus.FRAUD_REJECTED,
+          ).length,
+        };
+      }),
+    );
+  }
+}
