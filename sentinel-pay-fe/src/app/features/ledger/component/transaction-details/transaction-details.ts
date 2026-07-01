b7835ea021ae68a3c0d0941a19a5d5557f137380
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-01 23:10:09 +0530

    dialog created mat-dialog

--- sentinel-pay-fe/src/app/features/ledger/component/transaction-details/transaction-details.ts
+++ sentinel-pay-fe/src/app/features/ledger/component/transaction-details/transaction-details.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { LedgerResponse } from '../../model/ledger-response';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-transaction-details',
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatAnchor],
   templateUrl: './transaction-details.html',
   styleUrl: './transaction-details.css',
 })
