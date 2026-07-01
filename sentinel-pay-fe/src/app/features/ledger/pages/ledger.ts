7c0fa02d9b2e47e36ea5a91eed0e71cb76c256f9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-01 19:40:53 +0530

    move to page

---
+++ sentinel-pay-fe/src/app/features/ledger/pages/ledger.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ledger',
+  imports: [],
+  templateUrl: './ledger.html',
+  styleUrl: './ledger.css',
+})
+export class Ledger {
+
+}
