fcc690594279bedb08d6c28d79912febb24a5289
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-04 22:34:46 +0530

    add API for users

--- sentinel-pay-fe/src/app/core/config/api.config.ts
+++ sentinel-pay-fe/src/app/core/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
   auth: 'http://localhost:8080/api/auth',
+  user: 'http://localhost:8080/api/users',
   wallet: 'http://localhost:8080/api/wallets',
   payment: 'http://localhost:8080/api/payments',
   ledger: 'http://localhost:8080/api/ledger',
