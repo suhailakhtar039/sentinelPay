5b8d89bf0ed939f8c85434a2f963563c9f811f6e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-05 13:46:24 +0530

    add new provider

--- sentinel-pay-fe/src/app/app.config.ts
+++ sentinel-pay-fe/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +12,7 @@ import { authInterceptor } from './core/interceptors/auth-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])), // added by me
   ],
