aa8fd6c54a6ead5e9afea7ee371f18a139889bf8
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-06 22:56:56 +0530

    AnalyticsService created

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/service/AnalyticsService.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/service/AnalyticsService.java
@@ -1,4 +1,24 @@
 package com.paymentservice.service;
 
+import com.paymentservice.dto.analytics.AverageAmountResponse;
+import com.paymentservice.dto.analytics.DailyTransactionResponse;
+import com.paymentservice.dto.analytics.MonthlyVolumeResponse;
+import com.paymentservice.dto.analytics.OverviewAnalyticsResponse;
+import com.paymentservice.dto.analytics.PaymentStatusResponse;
+import com.paymentservice.dto.analytics.TopReceiverResponse;
+
+import java.util.List;
+
 public interface AnalyticsService {
+    OverviewAnalyticsResponse getOverviewAnalytics();
+
+    List<DailyTransactionResponse> getDailyTransactions();
+
+    List<MonthlyVolumeResponse> getMonthly();
+
+    List<PaymentStatusResponse> getPaymentStatusDistribution();
+
+    List<TopReceiverResponse> getTopReceivers();
+
+    AverageAmountResponse getAverageTransactionResponse();
 }
