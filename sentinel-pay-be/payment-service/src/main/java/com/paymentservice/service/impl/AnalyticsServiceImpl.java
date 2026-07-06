4d05856e45eb3331964d4361dd7c211c420673f7
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-06 19:39:33 +0530

    service created

---
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/service/impl/AnalyticsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.paymentservice.service.impl;
+
+import com.paymentservice.service.AnalyticsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsServiceImpl implements AnalyticsService {
+}
