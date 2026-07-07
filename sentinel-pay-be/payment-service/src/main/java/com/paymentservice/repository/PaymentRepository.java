b7143488fcbb266b2180278e41fe14e7f74a0c2e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-07 23:30:11 +0530

    getOverviewAnalytics function implemented in repo

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/repository/PaymentRepository.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.paymentservice.repository;
 
 import com.paymentservice.entity.Payment;
 import com.paymentservice.projection.analytics.OverviewAnalyticsProjection;
+import com.paymentservice.projection.analytics.PaymentStatusProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -19,15 +20,25 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     );
 
     @Query(value = """
-        SELECT
-            COUNT(*) AS totalPayments,
-            SUM(CASE WHEN status = 'COMPLETED' THEN 1 ELSE 0 END ) AS successfulPayments,
-            SUM(CASE WHEN status IN ('FAILED', 'FRAUD REJECTED') THEN 1 ELSE 0 END) AS failedPayments,
-            SUM(CASE WHEN status='PENDING' THEN 1 ELSE 0 END) AS pendingPayments,
-            COALESCE(SUM(amount), 0) AS totalVolume,
-            COALESCE(AVG(amount), 0) AS averageTransactionAmount
-        FROM payments
+            SELECT
+                COUNT(*) AS totalPayments,
+                SUM(CASE WHEN status = 'COMPLETED' THEN 1 ELSE 0 END ) AS successfulPayments,
+                SUM(CASE WHEN status IN ('FAILED', 'FRAUD REJECTED') THEN 1 ELSE 0 END) AS failedPayments,
+                SUM(CASE WHEN status='PENDING' THEN 1 ELSE 0 END) AS pendingPayments,
+                COALESCE(SUM(amount), 0) AS totalVolume,
+                COALESCE(AVG(amount), 0) AS averageTransactionAmount
+            FROM payments
             """, nativeQuery = true)
     OverviewAnalyticsProjection getOverviewAnalytics();
+
+    @Query(value = """
+            SELECT 
+                status AS status,
+                count(*) AS count
+                FROM payments
+                        GROUP BY status
+                        ORDER BY count DESC
+            """, nativeQuery = true)
+    List<PaymentStatusProjection> getPaymentStatusDistribution();
 
 }
