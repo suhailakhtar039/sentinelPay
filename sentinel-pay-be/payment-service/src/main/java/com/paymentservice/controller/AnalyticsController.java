2859e1da019c04bf33b99d02b38f44b992c48d6e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-07 23:11:02 +0530

    overview analytics endpoint created

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/controller/AnalyticsController.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/controller/AnalyticsController.java
@@ -1,9 +1,29 @@
 package com.paymentservice.controller;
 
+import com.paymentservice.dto.analytics.OverviewAnalyticsResponse;
+import com.paymentservice.service.AnalyticsService;
+import com.sentinelpay.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
-@RequestMapping("/api/analytics/")
+@RequiredArgsConstructor
+@RequestMapping("/api/analytics")
 public class AnalyticsController {
+    private final AnalyticsService analyticsService;
+
+    @GetMapping("/overview")
+    public ApiResponse<OverviewAnalyticsResponse> getOverviewAnalytics(){
+        return ApiResponse.<OverviewAnalyticsResponse>builder()
+                .success(true)
+                .message("Overview Analytics fetched successfully")
+                .data(analyticsService.getOverviewAnalytics())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
 }
