0b4598253c4735edad066aa468f088136b7f985a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-08 19:35:25 +0530

    payment status end point checked

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/controller/AnalyticsController.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/controller/AnalyticsController.java
@@ -1,6 +1,7 @@
 package com.paymentservice.controller;
 
 import com.paymentservice.dto.analytics.OverviewAnalyticsResponse;
+import com.paymentservice.dto.analytics.PaymentStatusResponse;
 import com.paymentservice.service.AnalyticsService;
 import com.sentinelpay.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +19,21 @@ public class AnalyticsController {
     private final AnalyticsService analyticsService;
 
     @GetMapping("/overview")
-    public ApiResponse<OverviewAnalyticsResponse> getOverviewAnalytics(){
+    public ApiResponse<OverviewAnalyticsResponse> getOverviewAnalytics() {
         return ApiResponse.<OverviewAnalyticsResponse>builder()
                 .success(true)
                 .message("Overview Analytics fetched successfully")
                 .data(analyticsService.getOverviewAnalytics())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @GetMapping("/payment-status")
+    public ApiResponse<List<PaymentStatusResponse>> getPaymentStatusDistribution() {
+        return ApiResponse.<List<PaymentStatusResponse>>builder()
+                .success(true)
+                .message("Payment status distribution fetched successfully")
+                .data(analyticsService.getPaymentStatusDistribution())
                 .timestamp(LocalDateTime.now())
                 .build();
     }
