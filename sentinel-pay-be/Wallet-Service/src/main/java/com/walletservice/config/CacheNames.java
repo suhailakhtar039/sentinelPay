bd6a4e90350a33ebb614b9f757b8be5958e24f27
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-12 15:23:49 +0530

    constants create

---
+++ sentinel-pay-be/Wallet-Service/src/main/java/com/walletservice/config/CacheNames.java
@@ -0,0 +1,9 @@
+package com.walletservice.config;
+
+public final class CacheNames {
+    public static final String WALLET = "wallet";
+    public static final String ANALYTICS = "analytics";
+    public static final String PROFILE = "profile";
+
+    private CacheNames() {}
+}
