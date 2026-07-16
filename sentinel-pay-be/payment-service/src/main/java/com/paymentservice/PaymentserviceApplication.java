f77ca66108ed0c10d8523fccd10735f814223df4
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-16 23:07:05 +0530

    enable cache in payment service

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/PaymentserviceApplication.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/PaymentserviceApplication.java
@@ -2,8 +2,10 @@ package com.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PaymentserviceApplication {
 
 	public static void main(String[] args) {
