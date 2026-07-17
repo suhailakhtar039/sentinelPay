c79d06d518ea630f2737ddfb308152f9f6c16d75
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-17 22:57:33 +0530

    remove unwanted constants

--- sentinel-pay-be/payment-service/src/main/java/com/paymentservice/config/CacheNames.java
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/config/CacheNames.java
@@ -1,9 +1,7 @@
 package com.paymentservice.config;
 
 public final class CacheNames {
-    public static final String WALLET = "wallet";
     public static final String ANALYTICS = "analytics";
-    public static final String PROFILE = "profile";
 
     private CacheNames() {}
 }
