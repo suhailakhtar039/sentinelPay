dbf56642e3c2d81578a9458076fb6d2c4144d8a5
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-18 12:51:07 +0530

    create analytics cache keys

---
+++ sentinel-pay-be/payment-service/src/main/java/com/paymentservice/config/AnalyticsCacheKeys.java
@@ -0,0 +1,13 @@
+package com.paymentservice.config;
+
+public final class AnalyticsCacheKeys {
+
+    private AnalyticsCacheKeys() {}
+
+    public static final String OVERVIEW = "overview";
+    public static final String PAYMENT_STATUS = "payment-status";
+    public static final String AVERAGE_AMOUNT = "average-amount";
+    public static final String DAILY_TRANSACTIONS = "daily-transactions";
+    public static final String MONTHLY_VOLUME = "monthly-volume";
+    public static final String TOP_RECEIVERS = "top-receivers";
+}
