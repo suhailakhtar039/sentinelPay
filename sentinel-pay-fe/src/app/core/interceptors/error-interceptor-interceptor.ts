cbaebbb71a4dd8ca17a36e65e53f6085a4cf2518
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-19 22:53:30 +0530

    error interceptor created

---
+++ sentinel-pay-fe/src/app/core/interceptors/error-interceptor-interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../services/token-storage';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const tokenStorage = inject(TokenStorageService);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      switch (error.status) {
+        case 401:
+          tokenStorage.logout();
+          router.navigate(['/login']);
+          break;
+
+        case 403:
+          router.navigate(['/login']);
+          break;
+      }
+      return throwError(() => error);
+    }),
+  );
+};
