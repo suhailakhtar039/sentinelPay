57bbfcbf6fd263f695854345e6c9c18dde01c3f5
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-22 23:14:04 +0530

    create dto dashboard-summary

--- sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
+++ sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
@@ -1,5 +1,8 @@
 import { DashboardAnalytics } from './dashboard-analytics.model';
+import { PaymentResponse } from '../../payment/model/payment-response';
 
 export interface DashboardSummary {
   analytics: DashboardAnalytics;
+
+  recentPayments: PaymentResponse[];
 }
