a363e3ea64b3ed5d12c5414cfcfc282b6655cf34
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-22 22:35:22 +0530

    modify dashboard summary

--- sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
+++ sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
@@ -1,21 +1,5 @@
-import { LedgerResponse } from '../../ledger/model/ledger-response';
-import { WalletResponse } from '../../../shared/models/wallet-response';
-import { PaymentResponse } from '../../payment/model/payment-response';
+import { DashboardAnalytics } from './dashboard-analytics.model';
 
 export interface DashboardSummary {
-  wallet: WalletResponse;
-
-  payments: PaymentResponse[];
-
-  transactions: LedgerResponse[];
-
-  recentPayments: PaymentResponse[];
-
-  walletBalance: number;
-
-  totalPayments: number;
-
-  successfulPayments: number;
-
-  failedPayments: number;
+  analytics: DashboardAnalytics;
 }
