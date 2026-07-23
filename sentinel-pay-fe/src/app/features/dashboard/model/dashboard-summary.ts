d9e5287c440c4f8cc43e8f54bbe7660be0fd5825
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-23 23:00:01 +0530

    dashboard summary received from service. Single endpoint not yet created

--- sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
+++ sentinel-pay-fe/src/app/features/dashboard/model/dashboard-summary.ts
@@ -1,8 +1,11 @@
 import { DashboardAnalytics } from './dashboard-analytics.model';
 import { PaymentResponse } from '../../payment/model/payment-response';
+import { WalletResponse } from '../../../shared/models/wallet-response';
 
 export interface DashboardSummary {
   analytics: DashboardAnalytics;
 
   recentPayments: PaymentResponse[];
+
+  wallet: WalletResponse;
 }
