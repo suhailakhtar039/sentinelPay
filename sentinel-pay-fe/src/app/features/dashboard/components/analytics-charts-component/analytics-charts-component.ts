8414af41d6abd028a8d414be28e33f265d2c21cf
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-23 23:41:39 +0530

    create analytics charts component

---
+++ sentinel-pay-fe/src/app/features/dashboard/components/analytics-charts-component/analytics-charts-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-charts-component',
+  imports: [],
+  templateUrl: './analytics-charts-component.html',
+  styleUrl: './analytics-charts-component.css',
+})
+export class AnalyticsChartsComponent {
+
+}
