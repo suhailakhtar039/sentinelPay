eed4c831338b503e8334d761cb803d7949a15f9e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-25 14:01:30 +0530

    using signal

--- sentinel-pay-fe/src/app/features/dashboard/components/analytics-charts-component/analytics-charts-component.ts
+++ sentinel-pay-fe/src/app/features/dashboard/components/analytics-charts-component/analytics-charts-component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { DashboardAnalytics } from '../../model/dashboard-analytics.model';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { createMonthlyChart } from './analytics-charts.config';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-analytics-charts-component',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, NgApexchartsModule],
   templateUrl: './analytics-charts-component.html',
   styleUrl: './analytics-charts-component.css',
 })
 export class AnalyticsChartsComponent {
-  @Input({ required: true })
-  analytics!: DashboardAnalytics;
+  analytics = input.required<DashboardAnalytics>();
+
+  monthlyChart = computed(() => createMonthlyChart(this.analytics().monthlyVolume));
 }
