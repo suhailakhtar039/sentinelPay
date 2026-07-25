2a22c951baaf80cadd4a6bbf4a239e6bbe2cdd82
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-25 12:51:34 +0530

    import necessary module

--- sentinel-pay-fe/src/app/features/dashboard/components/analytics-charts-component/analytics-charts-component.ts
+++ sentinel-pay-fe/src/app/features/dashboard/components/analytics-charts-component/analytics-charts-component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { DashboardAnalytics } from '../../model/dashboard-analytics.model';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-analytics-charts-component',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './analytics-charts-component.html',
   styleUrl: './analytics-charts-component.css',
 })
