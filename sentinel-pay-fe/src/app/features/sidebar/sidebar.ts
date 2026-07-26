bd388a3914a576e4923280ba4859fd72b800e585
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-07-26 13:29:24 +0530

    add session service for logout functionality

--- sentinel-pay-fe/src/app/features/sidebar/sidebar.ts
+++ sentinel-pay-fe/src/app/features/sidebar/sidebar.ts
@@ -4,7 +4,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
+import { SessionService } from '../../core/services/session-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -20,4 +22,19 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  constructor(
+    private authService: AuthService,
+    private sessionService: SessionService,
+    private router: Router,
+  ) {}
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => this.sessionService.logout(),
+      error: () => this.sessionService.logout(),
+    });
+
+    this.router.navigate(['/login']);
+  }
+}
